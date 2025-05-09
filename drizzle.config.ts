310ccff9ed9e0c3746900774a60a0f5d481885cc
StrigZ <mirgasan200@gmail.com>
2025-05-09 17:25:23 +0300

    Replace discord provider with google provider; change project name

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,5 +8,5 @@ export default {
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ['botfactory.io_*'],
+  tablesFilter: ['botfactory_*'],
 } satisfies Config;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,7 +8,7 @@ import { type AdapterAccount } from 'next-auth/adapters';
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `botfactory.io_${name}`);
+export const createTable = pgTableCreator((name) => `botfactory_${name}`);
 
 export const posts = createTable(
   'post',
