62f621c9e6998d6c635a2303afc9de35f6941d8c
StrigZ <mirgasan200@gmail.com>
2025-05-14 01:38:05 +0300

    Change sign up button to dashboard button if user logged in on landing page

--- src/components/LandingPage/Header/AuthButton.tsx
+++ src/components/LandingPage/Header/AuthButton.tsx
@@ -1,21 +1,21 @@
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 
-import { Button, buttonVariants } from '~/components/ui/button';
+import { buttonVariants } from '~/components/ui/button';
 
 export default function AuthButton() {
+  const { data: session } = useSession();
+
   return (
-    // <Button className="scale-95 cursor-pointer hover:scale-100 hover:shadow active:scale-105">
-    //   Sign Up
-    // </Button>
     <Link
-      href="/login"
+      href={!session ? '/login' : '/dashboard'}
       className={buttonVariants({
         variant: 'secondary',
         className:
           'scale-95 cursor-pointer hover:scale-100 hover:shadow active:scale-105',
       })}
     >
-      Sign Up
+      {!session ? 'Sign Up' : 'Go to Dashboard'}
     </Link>
   );
 }
