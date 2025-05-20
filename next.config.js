a992c8969d22e73106e2e2980edc9edb1fbd6f71
StrigZ <mirgasan200@gmail.com>
2025-05-20 09:22:35 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,9 +10,7 @@ const config = {
     ignoreDuringBuilds: true,
   },
   typescript: { ignoreBuildErrors: true },
-  experimental: {
-    serverComponentsExternalPackages: ['grammy'],
-  },
+  serverExternalPackages: ['grammy'],
 };
 
 export default config;
