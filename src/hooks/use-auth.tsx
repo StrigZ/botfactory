fdf2783c11e209522e9b9bfa3aebf5c9adb2627b
StrigZ <mirgasan200@gmail.com>
2025-10-27 08:30:50 +0300

    Add loading spinner when loading

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -1,3 +1,4 @@
+import { Loader } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useContext } from 'react';
 import { useEffect } from 'react';
@@ -26,11 +27,15 @@ export function withAuth<P extends object>(Component: React.ComponentType<P>) {
     }, [isAuthenticated, loading, router]);
 
     if (loading) {
-      return <div>Loading...</div>;
+      return (
+        <div className="absolute inset-0 flex items-center justify-center">
+          <Loader className="animate-spin" size={36} />
+        </div>
+      );
     }
 
     if (!isAuthenticated) {
-      return <div>UNAUTHoRIZED</div>;
+      return <div>UNAUTHORIZED</div>;
     }
 
     return <Component {...props} />;
