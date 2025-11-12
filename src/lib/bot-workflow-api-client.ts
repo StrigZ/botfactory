c0826030c376fbbd589820c6e4cfe5fdcd9fd74f
StrigZ <mirgasan200@gmail.com>
2025-11-12 15:10:36 +0300

    Create botWorkflowApiClient

---
+++ src/lib/bot-workflow-api-client.ts
@@ -0,0 +1,66 @@
+const API_URL = '/api/workflows';
+
+export type Workflow = {
+  id: number;
+  owner: number;
+  bot: number;
+  name: string;
+  created_at: string;
+};
+export type WorkflowWithNodes = Workflow & {
+  nodes: WorkflowNode[];
+  edges: WorkflowEdge[];
+};
+export type WorkflowNode = {
+  id: number;
+  workflow: number;
+  node_type: string;
+  name: string;
+  position: string;
+  data: string;
+};
+export type WorkflowEdge = {
+  id: number;
+  workflow: number;
+  source: number;
+  target: number;
+};
+export type UpdateBotWorkflowInput = {
+  id: string;
+  data: {
+    nodes: WorkflowNode[];
+    edges: WorkflowEdge[];
+  };
+};
+
+class BotWorkflowApiClient {
+  // TODO: make separate api client class
+  private async handleResponse<T>(res: Response): Promise<T> {
+    if (!res.ok) {
+      const errorBody = (await res.json()) as { message: string };
+      const message = errorBody.message;
+      throw new Error(`${res.status}: ${message}`);
+    }
+    return res.json() as Promise<T>;
+  }
+
+  async getById(id: string) {
+    const res = await fetch(`${API_URL}/${id}`);
+    return this.handleResponse<Workflow>(res);
+  }
+  async getByIdWithNodes(id: string) {
+    const res = await fetch(`${API_URL}/${id}`);
+    return this.handleResponse<WorkflowWithNodes>(res);
+  }
+  async update({ id, data: { nodes, edges } }: UpdateBotWorkflowInput) {
+    const fetchProps: RequestInit = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ edges, nodes }),
+    };
+    const res = await fetch(`${API_URL}/${id}`, fetchProps);
+    return this.handleResponse<Workflow>(res);
+  }
+}
+
+export const botWorkflowApiClient = new BotWorkflowApiClient();
