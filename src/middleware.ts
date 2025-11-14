b0ee8e870d984d728945be8c9590a3095ece7821
StrigZ <mirgasan200@gmail.com>
2025-11-14 19:13:19 +0300

    Update func name

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ import { getTokens, refreshToken } from './lib/auth';
 const protectedRoutes = ['/dashboard'];
 const publicRoutes = ['/login', '/signup', '/'];
 
-export default async function proxy(req: NextRequest) {
+export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isProtectedRoute = protectedRoutes.includes(path);
   const isPublicRoute = publicRoutes.includes(path);
