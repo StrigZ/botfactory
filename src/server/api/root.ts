cecfbf1866e96c980c8e35034c6e162792a3d7a4
StrigZ <mirgasan200@gmail.com>
2025-11-18 18:44:05 +0300

    Delete bot and workflow related trpc backend files

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,17 +1,11 @@
 import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
 
-import { botRouter } from './routers/bot';
-import { workflowRouter } from './routers/workflow';
-
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-  bot: botRouter,
-  workflow: workflowRouter,
-});
+export const appRouter = createTRPCRouter({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
