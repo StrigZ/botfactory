c18e8fae32c85726c3380c618f2857b24a141ab1
StrigZ <mirgasan200@gmail.com>
2025-11-21 17:05:53 +0300

    Fix state name

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -5,28 +5,20 @@ import { useEffect } from 'react';
 
 import { AuthContext } from '~/context/AuthContext';
 
-export function useAuth() {
-  const context = useContext(AuthContext);
-
-  if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-
-  return context;
-}
+export const useAuth = () => useContext(AuthContext);
 
 export function withAuth<P extends object>(Component: React.ComponentType<P>) {
   return function ProtectedRoute(props: P) {
-    const { isAuthenticated, loading } = useAuth();
+    const { isAuthenticated, isLoading } = useAuth();
     const router = useRouter();
 
     useEffect(() => {
-      if (!loading && !isAuthenticated) {
+      if (!isLoading && !isAuthenticated) {
         void router.push('/login');
       }
-    }, [isAuthenticated, loading, router]);
+    }, [isAuthenticated, isLoading, router]);
 
-    if (loading) {
+    if (isLoading) {
       return (
         <div className="absolute inset-0 flex items-center justify-center">
           <Loader className="animate-spin" size={36} />
