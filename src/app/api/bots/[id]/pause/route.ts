b6cefd0419199dfa492626c08bbaa794a0f8e3ec
StrigZ <mirgasan200@gmail.com>
2025-11-30 07:31:13 +0300

    Use djangoFetch in all api routes

--- src/app/api/bots/[id]/pause/route.ts
+++ src/app/api/bots/[id]/pause/route.ts
@@ -1,10 +1,6 @@
-import { cookies } from 'next/headers';
 import type { NextRequest } from 'next/server';
 
-import { env } from '~/env';
-import type { Bot } from '~/lib/bot-api-client';
-
-const API_URL = env.API_URL;
+import { djangoFetch } from '~/lib/django-fetch';
 
 export async function POST(
   _: NextRequest,
@@ -13,34 +9,5 @@ export async function POST(
   // eslint-disable-next-line @typescript-eslint/await-thenable
   const { id } = await params;
 
-  const requestOption: RequestInit = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Cookie: (await cookies()).toString(),
-      credentials: 'include',
-    },
-  };
-
-  try {
-    const res = await fetch(`${API_URL}/bots/${id}/undeploy/`, requestOption);
-
-    if (!res.ok) {
-      throw new Error('Error during /api/bots/id api call: ' + res.statusText);
-    }
-
-    const data = (await res.json()) as Bot;
-
-    return Response.json(data);
-  } catch (e) {
-    console.error(e);
-    return Response.json(
-      {
-        message: 'Unexpected error occurred during bot pausing.',
-      },
-      {
-        status: 500,
-      },
-    );
-  }
+  return await djangoFetch(`/bots/${id}/undeploy/`, { method: 'POST' });
 }
--- src/app/api/bots/route.ts
+++ src/app/api/bots/route.ts
@@ -1,45 +1,13 @@
-import { cookies } from 'next/headers';
 import type { NextRequest } from 'next/server';
 
-import { env } from '~/env';
-import type { Bot, CreateBotInput } from '~/lib/bot-api-client';
 import { fetchBots } from '~/lib/dal';
-import { handleApiRequest } from '~/lib/django-fetch';
-
-const API_URL = env.API_URL;
+import { djangoFetch, handleApiRequest } from '~/lib/django-fetch';
 
 export async function POST(request: NextRequest) {
-  const requestData = (await request.json()) as CreateBotInput;
-
-  const requestOption: RequestInit = {
+  return await djangoFetch('/bots/', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Cookie: (await cookies()).toString(),
-      credentials: 'include',
-    },
-    body: JSON.stringify(requestData),
-  };
-
-  try {
-    const res = await fetch(`${API_URL}/bots/`, requestOption);
-
-    if (!res.ok) {
-      throw new Error('Error during /api/bots api call: ' + res.statusText);
-    }
-
-    const data = (await res.json()) as Bot;
-
-    return Response.json(data);
-  } catch (e) {
-    console.error(e);
-    return Response.json(
-      {
-        message: 'Unexpected error occurred during bot creation.',
-      },
-      { status: 500 },
-    );
-  }
+    body: JSON.stringify(await request.json()),
+  });
 }
 
 export async function GET() {
